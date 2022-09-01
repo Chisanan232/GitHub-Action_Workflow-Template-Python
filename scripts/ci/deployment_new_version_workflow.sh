72e64253425562734f7b72d4be6a5c1870e1c504
Chisanan232 <chi10211201@cycu.org.tw>
2022-09-01 09:07:02 +0800

    [Breaking Change] (config) Adjust the log message.

--- scripts/ci/deployment_new_version_workflow.sh
+++ scripts/ci/deployment_new_version_workflow.sh
@@ -4,21 +4,21 @@ debug_mode=$1
 
 final_release_type=$RELEASE_TYPE
 if [ "$final_release_type" == "Pre" ]; then
-    echo "It detects Pre-Release flag."
+    echo "💤 It detects Pre-Release flag. So it does NOT do anything in deployment process."
 else
-    echo "It detects Official-Release flag."
+    echo "📬 It detects Official-Release flag."
     if [ "$debug_mode" == true ]; then
         echo " 🔍👀[DEBUG MODE] Create new git branch for the new version $final_release_type."
     else
-        echo "Add git remote reference."
         git remote add github-action_workflow-template https://github.com/Chisanan232/GitHub-Action-Template-Python.git
-        echo "Check all git remote reference."
+        echo "🔗📄 Add git remote reference."
         git remote -v
-        echo "Create a new git branch as version."
+        echo "🔍 Check all git remote reference."
         git checkout -b "v$final_release_type"
-        echo "Push the source code as a branch with one specific version to the GitHub."
+        echo "⛓ Create a new git branch as version."
         git push -u github-action_workflow-template "v$final_release_type"
+        echo "🍻🎉 Push the source code as a branch with one specific version to the GitHub."
     fi
 fi
 
-echo "Done!"
+echo "🎊🥂 Done!"
