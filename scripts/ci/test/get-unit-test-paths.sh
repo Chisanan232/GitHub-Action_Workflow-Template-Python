00c48d6de8e22af09fc74d71fdde4f7cd8b818cf
Chisanan232 <chi10211201@cycu.org.tw>
2023-10-15 22:27:46 +0800

    ✏️  Modify the shell script to get test modules without adding path manually (not finish).

--- scripts/ci/test/get-unit-test-paths.sh
+++ scripts/ci/test/get-unit-test-paths.sh
@@ -3,9 +3,13 @@
 set -ex
 runtime_os=$1
 
-declare -a init_tests
+declare base_directory
+base_directory=test/unit_test/
+
+declare -a all_tests
 
 getalltests() {
+    # Get all test items from the module (.py files)
     declare -a testpatharray=( $(ls -F "$1" | grep -v '/$' | grep -v '__init__.py' | grep -v 'test_config.py' | grep -v -E '^_[a-z_]{1,64}.py' | grep -v '__pycache__'))
 
     declare -a alltestpaths
@@ -13,14 +17,35 @@ getalltests() {
         alltestpaths[$i]=$1${testpatharray[$i]}
     done
 
-    init_tests=("${alltestpaths[@]}")
+    all_tests+=("${alltestpaths[@]}")
 }
 
-init_path=test/unit_test/
+get_all_test_subpackage() {
+    # Get all test directories (python subpackage)
+    # Note: use dept-first search algorithm
+    declare -a testpatharray=( $(ls -d "$1/*/" | grep -v '/$' | grep -v '__init__.py' | grep -v 'test_config.py' | grep -v -E '^_[a-z_]{1,64}.py' | grep -v '__pycache__'))
 
-getalltests $init_path
+    if echo ${#testpatharray[@]} | grep -q "0";
+    then
+        # No any directory under this path, try to get the test modules
+        getalltests "$1"
+    else
+        # Has some directories under this path, keep searching
+#        declare -a alltestpaths
+        for (( i = 0; i < ${#testpatharray[@]}; i++ )) ; do
+            get_all_test_subpackage testpatharray[$i]
+#            alltestpaths[$i]=$1${testpatharray[$i]}
+        done
+    fi
 
-dest=( "${init_tests[@]}" )
+#    all_tests=("${alltestpaths[@]}")
+}
+
+#init_path=test/unit_test/
+
+getalltests $base_directory
+
+dest=( "${all_tests[@]}" )
 
 
 if echo "$runtime_os" | grep -q "windows";
