561e6208856ef951afe73ae381ffbd81d9a7d9bd
Chisanan232 <chi10211201@cycu.org.tw>
2023-10-17 09:54:18 +0800

    🚮 Remove unused shell which has been commented out and won't be used.

--- scripts/ci/test/get-unit-test-paths.sh
+++ scripts/ci/test/get-unit-test-paths.sh
@@ -42,7 +42,6 @@ get_all_test_subpackage() {
         printf "Start to find under path $test_path" | jq -R . | jq -cs .
         printf '%s\n' "${all_test_subpkgs[@]}" | jq -R . | jq -cs .
         declare -a test_subpkg_array=( $(ls -d $test_path | grep -v '__pycache__') )
-    #    declare ls_exit_code=$?
 
         echo "[DEBUG in finally] test_subpkg_array: $test_subpkg_array"
 
@@ -51,18 +50,7 @@ get_all_test_subpackage() {
             printf "No any directory under this path, try to get the test modules" | jq -R . | jq -cs .
             # No any directory under this path, try to get the test modules
             all_test_subpkgs+=( "${test_subpkg_array[@]}" )
-    #        for test_subpkg in ${all_test_subpkgs};
-    #        do
-    #            printf "Start to find under path $test_subpkg" | jq -R . | jq -cs .
             get_all_test_subpackage $(( $index + 1 ))
-      #            alltestpaths[$i]=$1${testpatharray[$i]}
-    #        done
-    #        getalltests "$1"
-    #    if echo $ls_exit_code | grep -q 1;
-    #    then
-    #        # No any directory under this path, try to get the test modules
-    #        all_test_subpkgs+=("$1")
-    #        getalltests "$1"
         else
             printf "Has some directories under this path, keep searching" | jq -R . | jq -cs .
             # Has some directories under this path, keep searching
@@ -72,28 +60,15 @@ get_all_test_subpackage() {
             else
                 printf "Done to find under path $1" | jq -R . | jq -cs .
             fi
-    #        getalltests "$1"
-    ##        declare -a alltestpaths
-    #        for (( i = 0; i < ${#test_subpkg_array[@]}; i++ )) ; do
-    #            get_all_test_subpackage "${test_subpkg_array[$i]}"
-    #  #            alltestpaths[$i]=$1${testpatharray[$i]}
-    #        done
         fi
     else
         printf "Path '$test_subpkg' is empty, ignore to run." | jq -R . | jq -cs .
     fi
-
-#    all_tests=("${alltestpaths[@]}")
 }
 
-#init_path=test/unit_test/
-
-#getalltests $base_directory
 get_all_test_subpackage
 
 printf '%s\n' "${all_test_subpkgs[@]}" | jq -R . | jq -cs .
-
-#echo "[DEBUG in finally] all_test_subpkgs: ${all_test_subpkgs}"
 
 # Get all test modules under these test sub-pacakges
 for test_subpkg in "${all_test_subpkgs[@]}";
