1d717eb6413b670ff84e90171dba6457ee024610
Chisanan232 <chi10211201@cycu.org.tw>
2024-03-15 20:52:40 +0800

    ➕ Add new test type *system test* for organizing the test coverage report.

--- scripts/ci/combine_coverage_reports.sh
+++ scripts/ci/combine_coverage_reports.sh
@@ -6,14 +6,14 @@ test_type=$1
 
 coveragedatafile=".coverage.$test_type"
 
-if [ "$test_type" == "unit-test" ] || [ "$test_type" == "integration-test" ];
+if [ "$test_type" == "unit-test" ] || [ "$test_type" == "integration-test" ] || [ "$test_type" == "system-test" ];
 then
   coverage combine --data-file="$coveragedatafile" .coverage."$test_type".*
 elif [ "$test_type" == "all-test" ];
 then
   coverage combine --data-file="$coveragedatafile" .coverage.*
 else
-  echo "❌ It doesn't support $test_type currently. Please change to use options 'unit-test', 'integration-test' or 'all-test'." && exit 1
+  echo "❌ It doesn't support $test_type currently. Please change to use options 'unit-test', 'integration-test', 'system-test' or 'all-test'." && exit 1
 fi
 
 coverage report -m --data-file="$coveragedatafile"
