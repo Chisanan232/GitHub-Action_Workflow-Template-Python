5947315a9f6f8000f63a47ee104d76dda7c3ee63
Chisanan232 <chi10211201@cycu.org.tw>
2024-11-23 21:19:28 +0800

    ✏️ Let the CI process supports parsing and generating more detail version info ('X' -> 'X.X').

--- scripts/ci/generate_release_info.sh
+++ scripts/ci/generate_release_info.sh
@@ -4,5 +4,5 @@ Input_Arg_Debug_Mode=$2
 release=$(bash ./scripts/ci/build_git-tag_or_create_github-release.sh "$Input_Arg_Project_Type" "$Input_Arg_Debug_Mode")
 echo "📄 Release log: $release"
 
-release_version=$(echo "$release" | grep -E "\[GitHub Action - Reusable workflow\] \[Final Running Result\] (Official\-Release and version: ([0-9]{1,})|(Pre\-Release))" | grep -E -o "(([0-9]{1,})|(Pre\-Release))")
+release_version=$(echo "$release" | grep -E "\[GitHub Action - Reusable workflow\] \[Final Running Result\] (Official\-Release and version: ([0-9]{1,}\.[0-9]{1,})|(Pre\-Release))" | grep -E -o "(([0-9]{1,}\.[0-9]{1,})|(Pre\-Release))")
 echo " 📲 Target version which would be pass to deployment process: $release_version"
