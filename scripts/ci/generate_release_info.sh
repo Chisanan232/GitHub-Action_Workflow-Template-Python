1741a9e0e0742d8a7d58a46fa47315d69a05e02c
Chisanan232 <chi10211201@cycu.org.tw>
2024-11-23 21:15:29 +0800

    ♻️ Refactor the CI step about using shell script to run the process generates release version info.

---
+++ scripts/ci/generate_release_info.sh
@@ -0,0 +1,8 @@
+Input_Arg_Project_Type=$1
+Input_Arg_Debug_Mode=$2
+
+release=$(bash ./scripts/ci/build_git-tag_or_create_github-release.sh "$Input_Arg_Project_Type" "$Input_Arg_Debug_Mode")
+echo "📄 Release log: $release"
+
+release_version=$(echo "$release" | grep -E "\[GitHub Action - Reusable workflow\] \[Final Running Result\] (Official\-Release and version: ([0-9]{1,})|(Pre\-Release))" | grep -E -o "(([0-9]{1,})|(Pre\-Release))")
+echo " 📲 Target version which would be pass to deployment process: $release_version"
